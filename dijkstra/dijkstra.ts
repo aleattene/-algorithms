686c43e9b58ebb603b1ba33101324cfe68330a32
aleattene <attene.tomorrowdevs@gmail.com>
2024-05-23 12:14:08 +0200

    fix: add node sorting based on the shortest path

--- dijkstra/dijkstra.ts
+++ dijkstra/dijkstra.ts
@@ -2,18 +2,20 @@ interface Graph {
     [key: string]: { [key: string]: number };
 }
 
+
 export function getDijkstraMinPath(distancesGraph: Graph, start: string, end: string) {
     const distances: { [key: string]: [number, string] } = {};
 
     for(const node in distancesGraph) {
         distances[node] = [ Infinity, ""];
     }
-    distances[start] = [0, start];
+    distances[start] = [0, start]; // { A: [0, "A"], B: [Infinity, ""], ... , [Infinity, ""] }
 
-    const nodes = Object.keys(distancesGraph);
+    const nodes = Object.keys(distancesGraph); // [ 'A', 'B', ... , 'Z']
 
     while(nodes.length) {
-        const currentNode = nodes[0];
+        nodes.sort((a, b) => distances[a][0] - distances[b][0]);
+        const currentNode = nodes[0]; // A, B, C, D, ... , Z
         const currentDistance = distances[currentNode][0];
         for (const neighbor in distancesGraph[currentNode]) {
             const newDistance = currentDistance + distancesGraph[currentNode][neighbor];
@@ -22,13 +24,22 @@ export function getDijkstraMinPath(distancesGraph: Graph, start: string, end: st
                 distances[neighbor][1] = currentNode;
             }
         }
-        nodes.shift();
+        nodes.shift(); // Remove visited node
     }
 
-    const minPath = { path : [end], distance : distances[end][0]};
+    const minPath = {
+        path : [end],
+        distance : 0
+    };
+
     while (end !== start) {
         end = distances[end][1];
         minPath.path.unshift(end);
+    }
+
+    // Calculate distance
+    for(let i = 0; i < minPath.path.length - 1; i++) {
+        minPath.distance += distancesGraph[minPath.path[i]][minPath.path[i + 1]];
     }
 
     return minPath;
